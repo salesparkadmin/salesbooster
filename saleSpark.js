46a070e400c33f6e1bdfb2d944136ef9c3ce35ce
salespark <60234730+salespark@users.noreply.github.com>
2020-06-22 22:48:36 +0500

    Update saleSpark.js

--- saleSpark.js
+++ saleSpark.js
@@ -44,7 +44,7 @@ function removeGDPR() {
 function globalJavascript() {
 
     const webApi = "https://" + window.location.hostname + "/apps/storefront/api/storefront/";
-    const scriptBase = "https://cdn.jsdelivr.net/gh/salespark/salesbooster@1.1.5/";
+    const scriptBase = "https://cdn.jsdelivr.net/gh/salespark/salesbooster@1.1.7/";
     var sweetAlertIncluded = false;
     var store = {};
     this.init = function (callback, callbackArgs) {
